c8a9cc23f87c43049cad5367566a9de87c43a8e3
Jian <Jian@user>
2018-11-13 21:27:22 +0800

    1st commit

---
+++ DataStructuresAssignment/src/datastructuresassignment/DataStructuresAssignment.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructuresassignment;
+
+/**
+ *
+ * @author Jian
+ */
+public class DataStructuresAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Testing
+        System.out.print("Testing to commit.");
+        
+        System.out.print("Testing to commit.");
+        System.out.print("Testing to commit.");
+    }
+    
+}
